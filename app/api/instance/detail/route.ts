d11277af84f584fe509b0d0e0c365130e73f9551
sythanh2001 <thanhns2k1@gmail.com>
2023-10-13 11:51:24 +0700

    save

--- app/api/instance/detail/route.ts
+++ app/api/instance/detail/route.ts
@@ -23,11 +23,11 @@ export async function GET(req: NextRequest) {
     (x) => x.key == "domain"
   )?.value;
   let cloudflare = undefined;
-  // if (domainTemp) {
-  //   cloudflare = (await cf.dnsRecords.browse(CLOUDFLARE_ZONE_ID)).result?.find(
-  //     (x: any) => x.name == domainTemp
-  //   );
-  // }
+  if (domainTemp) {
+    cloudflare = (await cf.dnsRecords.browse(CLOUDFLARE_ZONE_ID)).result?.find(
+      (x: any) => x.name == domainTemp
+    );
+  }
 
   return NextResponse.json({ instance, cloudflare });
 }
